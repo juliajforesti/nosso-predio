c3607d25a473f4cabcce4ea626bf4ab6e24d5f68
Julia Foresti <juliajforesti@gmail.com>
2020-05-15 17:38:03 -0300

    cors updated for heroku

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use(passport.session());
 app.use(
   cors({
     credentials: true,
-    origin: 'http://localhost:3000' // <== this will be the URL of our React app (it will be running on port 3000)
+    origin: ['http://localhost:3000', 'https://nosso-predio.herokuapp.com'] // <== this will be the URL of our React app (it will be running on port 3000)
   })
 );
 
